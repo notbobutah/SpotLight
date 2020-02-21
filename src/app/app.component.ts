66460f053b7152f8f0b9b424334a006f18a84ddc
notbobutah <rmackay@neovest.com>
2020-02-20 20:07:47 -0700

    first commit 02/20/2020

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NodeModel, ConnectorModel, Connector } from '@syncfusion/ej2-angular-diagrams';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,60 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'spotlight';
+   public nodes: NodeModel[] = [
+     { 
+        //Define unique id for each shape
+         id: 'Start', offsetY: 50, annotations: [{ content: 'Start' }], 
+          //Define the shape type and flow shape
+         shape: { type: 'Flow', shape: 'Terminator' } 
+      },
+      {
+         id: 'Init', offsetY: 150, annotations: [{ content: 'var i = 0;' }],
+          shape: { type: 'Flow', shape: 'Process' } 
+       },
+      {
+          id: 'Condition', offsetY: 250, annotations: [{ content: 'i < 10?' }], shape: { type: 'Flow', shape: 'Decision' },
+          
+        },
+        { id: 'Print', offsetY: 350, annotations: [{ content: 'print(\'Hello!!\');' }], shape: { type: 'Flow', shape: 'PreDefinedProcess' } },
+        {
+          id: 'Increment', offsetY: 450, annotations: [{ content: 'i++;' }], shape: { type: 'Flow', shape: 'Process' },
+          
+        },
+        {
+          id: 'End', offsetY: 550, annotations: [{ content: 'End' }], shape: { type: 'Flow', shape: 'Terminator' },
+         
+        },
+    ];
+        public connectors: ConnectorModel[] = [
+          { 
+              //Define unique ID for connectors
+             id: 'connector1',
+             //SourceID and targetID property is used to define the relationship between shapes
+             sourceID: 'Start', targetID: 'Init' 
+           },
+         {
+            id: 'connector3', sourceID: 'Condition', targetID: 'Print',
+            annotations: [{ content: 'Yes' }]
+          },
+          {
+            id: 'connector4', sourceID: 'Condition', targetID: 'End',  
+            annotations: [{ content: 'No' }]
+          },
+          { id: 'connector5', sourceID: 'Print', targetID: 'Increment' },
+          {
+            id: 'connector6', sourceID: 'Increment', targetID: 'Condition',  
+          }
+        ];
+        public nodeDefaults(obj: NodeModel) : NodeModel {
+            let node: NodeModel = {};
+            node.height = 50;
+            node.width = 140;
+            node.offsetX = 300;
+            return node;
+          } 
+        public connDefaults(obj: Connector) : void {
+            obj.type = 'Orthogonal';
+            obj.targetDecorator = { shape: 'Arrow', width: 10, height: 10 };
+          }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { DiagramModule } from '@syncfusion/ej2-angular-diagrams';
 import { NgModule } from '@angular/core';
-
+ 
 import { AppComponent } from './app.component';
-
+ 
+/**
+ * Module
+ */
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports: [
+      BrowserModule, DiagramModule
+  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+  providers: [ ],
 })
+ 
 export class AppModule { }
+ 
